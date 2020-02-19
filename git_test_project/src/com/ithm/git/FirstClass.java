1af8c4db761f0ee3fa2bd929f924b122e38de9d2
offcn-java <myjava@qq.com>
2020-02-19 15:49:22 +0800

    本地工程添加到仓库

---
+++ git_test_project/src/com/ithm/git/FirstClass.java
@@ -0,0 +1,20 @@
+package com.ithm.git;
+
+/**
+ * @ClassNameFirstClass
+ * @Description
+ * @Author
+ * @Date2020/2/19 15:39
+ * @Version V1.0
+ **/
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+
+}
